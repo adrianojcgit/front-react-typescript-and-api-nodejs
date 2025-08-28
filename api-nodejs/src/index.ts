a837d44f1547e89558a842dab7b3ae74bdfbcb24
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-08-28 19:00:15 -0300

    Adicionar dependências do BD e ORM

--- api-nodejs/src/index.ts
+++ api-nodejs/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response} from 'express';
+import { AppDataSource } from "./data-source";
 
 //criar a aplicação express
 const app = express();
@@ -10,3 +11,10 @@ app.get("/", ( req: Request, res: Response ) => {
 app.listen(8080, () => {
     console.log("Servidor iniciado na porta 8080: http://localhost:8080");
 });
+
+//inicializar a conexão com o Banco
+AppDataSource.initialize()
+.then(() => {
+    console.log("Conexão com o banco de dados realizada com sucesso!");
+})
+.catch((error) => console.log("Erro na conexão com o banco de dados: ", error));
