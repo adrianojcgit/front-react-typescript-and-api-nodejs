458d336dec72cd837dc664b1270ab4f3dc03187f
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-09-02 09:42:38 -0300

    Excluir o projeto web-react.

--- web-react/src/services/api.tsx
+++
@@ -1,9 +0,0 @@
-import axios, {AxiosInstance} from "axios"; 
-	
-const instance: AxiosInstance = axios.create({
-    baseURL: "http://localhost:8080",
-    headers: {
-        "Content-Type": "application/json"
-    }
-});
-export default instance;
