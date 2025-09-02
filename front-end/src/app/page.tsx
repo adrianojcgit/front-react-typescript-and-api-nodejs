d51084b8162b5db4b46d8db24803debb2d880368
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-09-02 18:36:35 -0300

    Adicionar o tailwind para a pagina lisar  e cadastrar.

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import Menu from "@/app/components/Menu";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col h-screen bg-gray-100">
+      <Menu/>
       <p>Bem-Vindos!!</p>
       <Link href="/users/list">Usuários</Link> 
     </div>
--- front-end/src/app/users/list/page.tsx
+++ front-end/src/app/users/list/page.tsx
@@ -2,14 +2,15 @@
 import instance from "@/services/api";
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import Menu from "@/app/components/Menu";
 
-interface User{
+interface User {
     id: number,
     name: string,
     email: string
 }
 
-export default function Users(){
+export default function Users() {
 
     const [error, setError] = useState<string | null>(null);
     const [users, setUsers] = useState<User[]>([]);
@@ -23,40 +24,56 @@ export default function Users(){
         }
     }
 
-        //Hook parra buscar os dados naa primeira renderização: ocorerá aapenas uma vez.
+    //Hook parra buscar os dados naa primeira renderização: ocorerá aapenas uma vez.
     useEffect(() => {
         fetchUsers();
     }, []);
 
-    return(
-        <div>
-            <h1>Listar Usuários</h1>          
-            <Link href={'/users/create'}>Cadastrar</Link>
-            {error && <p style={{color: "#f00"}}>{error}</p>}
-            <table>
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Nome</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>    
-                <tbody>  
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    {/*
-                    { users.map((user) => (
-                        <tr key={user.id}>
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
+    return (
+        <div className="flex flex-col h-screen bg-gray-100">
+            {/* Menu Superior*/}
+            <Menu />
+            {/* Conteúdo principal */}
+            <div className="flex-1 px-2 py-6 max-w-6xl mx-auto w-full">
+                <div className="flex justify-between items-center mb-6">
+                    <h1 className="text-2xl font-bold">Listar Usuários</h1>
+                    <Link href={'/users/create'} className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600">Cadastrar</Link>
+                </div>
+                {/* Exibe mensagem de erro */}
+                {error && <p className="text-red-500 mt-4">{error}</p>}
+                <div className="mt-6 bg-white shadow-md rounded-lg p-6">
+                {/* Tabela */}
+                <table className="w-full border-collapse">
+                    <thead>
+                        <tr className="bg-gray-200">
+                            <th className="border p-3 text-left">Id</th>
+                            <th className="border p-3 text-left">Nome</th>
+                            <th className="border p-3 text-left">Email</th>
+                            <th className="border p-3 text-center">Ações</th>
                         </tr>
-                        ))}*/}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                         
+                        <tr className="hover:bg-gray-100">
+                            <td className="border p-3">1</td>
+                            <td className="border p-3"></td>
+                            <td className="border p-3"></td>
+                            <td className="border p-3"></td>
+                        </tr>
+                       {/*
+                    { users.map((user) => (
+                        <tr key={user.id}  className="hover:bg-gray-100">
+                            <td className="border p-3">{user.id}</td>
+                            <td className="border p-3">{user.name}</td>
+                            <td className="border p-3">{user.email}</td>
+                            <td className="border p-3"></td>
+                        </tr>
+                        ))}
+                       */} 
+                    </tbody>
+                </table>
+                </div>
+            </div>
         </div>
     )
 }
